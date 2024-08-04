b3665de8c968dc177fc3296353d7f03c0261d8c8
RonaldoBaker <r.remario.baker@gmail.com>
2024-08-04 18:43:34 +0100

    Try to remove print lines and comments again

--- calculator.py
+++ calculator.py
@@ -2,16 +2,8 @@ def add(x, y):
     return x + y
 
 
-print(add(5, 4))
-
-
 def sub(x, y):
     return x - y
-
-
-print("Hello I'm going to test stashing here")
-
-# New code on test-branch
 
 
 def mult(x, y):
@@ -20,4 +12,3 @@ def mult(x, y):
 
 def div(x, y):
     return x / y
-
